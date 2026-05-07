da9d7c56cf1438d1894a2b3bf5d806bbc5365eb9
Kishanr707 <kishanramesh136@gmail.com>
2026-05-08 01:34:05 +0530

    Refactor EmployeeServlet by removing unnecessary lines

--- lab8/EmployeeServlet.java
+++ lab8/EmployeeServlet.java
@@ -10,34 +10,21 @@ import java.io.IOException;
 import java.io.*;
 import java.sql.*;
 
-/**
- * Servlet implementation class EmployeeServlet
- */
 @WebServlet("/EmployeeServlet")
 public class EmployeeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public EmployeeServlet() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
 		int id = Integer.valueOf(request.getParameter("id"));
